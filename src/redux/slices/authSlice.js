b117c095cdd9697f9070917d209ea993e279a46a
hyeonw8 <whgusrud223@gmail.com>
2024-06-03 20:01:46 +0900

    ✨ update  : authSlice 기본 설정 추가

--- src/redux/slices/authSlice.js
+++ src/redux/slices/authSlice.js
@@ -1,79 +1,26 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import api from '../../api';
+import { createSlice } from '@reduxjs/toolkit';
 
-// 회원가입 비동기 작업 정의
-export const signUp = createAsyncThunk(
-  'auth/signUp',
-  async ({ email, password, username, profilePictureFile }, { rejectWithValue }) => {
-    try {
-      const signUpData = await api.auth.signUp(email, password, username, profilePictureFile);
-      return signUpData.user;
-    } catch (error) {
-      return rejectWithValue(error.message);
+const initialState = {
+  user: null,
+  loading: false,
+  error: null
+};
+
+export const authSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    setLoading(state, action) {
+      state.loading = action.payload;
+    },
+    setUser(state, action) {
+      state.user = action.payload;
+    },
+    setError(state, action) {
+      state.error = action.payload;
     }
   }
-);
-
-// 로그인 비동기 작업 정의
-export const signIn = createAsyncThunk('auth/signIn', async ({ email, password }, { rejectWithValue }) => {
-  try {
-    const signInData = await api.auth.signIn(email, password);
-    return signInData.user;
-  } catch (error) {
-    return rejectWithValue(error.message);
-  }
 });
 
-// 로그아웃 비동기 작업 정의
-export const signOut = createAsyncThunk('auth/signOut', async (_, { rejectWithValue }) => {
-  try {
-    await api.auth.signOut();
-    return {};
-  } catch (error) {
-    return rejectWithValue(error.message);
-  }
-});
-
-const authSlice = createSlice({
-  name: 'auth',
-  initialState: { user: null, status: 'idle', error: null },
-  reducers: {},
-  extraReducers: (builder) => {
-    builder
-      .addCase(signUp.pending, (state) => {
-        state.status = 'loading'; // 회원가입 요청 중 상태
-      })
-      .addCase(signUp.fulfilled, (state, action) => {
-        state.status = 'succeeded'; // 회원가입 성공 상태
-        state.user = action.payload; // 유저 정보 저장
-      })
-      .addCase(signUp.rejected, (state, action) => {
-        state.status = 'failed'; // 회원가입 실패 상태
-        state.error = action.payload; // 에러 메시지 저장
-      })
-      .addCase(signIn.pending, (state) => {
-        state.status = 'loading'; // 로그인 요청 중 상태
-      })
-      .addCase(signIn.fulfilled, (state, action) => {
-        state.status = 'succeeded'; // 로그인 성공 상태
-        state.user = action.payload; // 유저 정보 저장
-      })
-      .addCase(signIn.rejected, (state, action) => {
-        state.status = 'failed'; // 로그인 실패 상태
-        state.error = action.payload; // 에러 메시지 저장
-      })
-      .addCase(signOut.pending, (state) => {
-        state.status = 'loading'; // 로그아웃 요청 중 상태
-      })
-      .addCase(signOut.fulfilled, (state) => {
-        state.status = 'succeeded'; // 로그아웃 성공 상태
-        state.user = null; // 유저 정보 제거
-      })
-      .addCase(signOut.rejected, (state, action) => {
-        state.status = 'failed'; // 로그아웃 실패 상태
-        state.error = action.payload; // 에러 메시지 저장
-      });
-  }
-});
-
+export const { setLoading, setError, setUser } = authSlice.actions;
 export default authSlice.reducer;
