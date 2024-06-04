271bb12203805d73da7d0e52eef2a0b73e4a1a44
hyeonw8 <whgusrud223@gmail.com>
2024-06-05 00:04:48 +0900

    ✨ update : useSelector를 이용하여 사용자 id 받아오기
    사용자 id 이용하여 UserInfo, PostList 구성

--- src/pages/MyPage/MyPage.jsx
+++ src/pages/MyPage/MyPage.jsx
@@ -8,26 +8,39 @@ import {
   StPostListTitle
 } from './MyPage.styled';
 import PostList from '../../components/posts/PostList';
-import UserInfo from '../../components/ui/UserInfo/UserInfo';
-import { useParams } from 'react-router-dom';
+import UserInfo from '../../components/ui/UserInfo';
+import { useSelector } from 'react-redux';
+
 
 const MyPage = () => {
-  const params = useParams();
-  console.log('params', params);
-  //const userId = 'b3a9af59-c376-4036-b98d-718661a3c7cc';
+  const user = useSelector((state) => state.auth.user);
+
+  console.log('user', user);
+
+  if (!user) {
+    return <p>로그인이 필요합니다.</p>;
+  }
+
+  const userId = user.id;
+  console.log('userId', userId);
 
   return (
+    
     <StMyPageWrapper>
+      <div>
+        <h1>{user.email}님의 페이지</h1>
+        <p>사용자 UUID: {user.id}</p>
+      </div>
       <StDivProfile>
         <StTitle>Profile</StTitle>
         <StAccount>
-          <UserInfo userId={params.userId} />
+            <UserInfo userId={userId} />
         </StAccount>
       </StDivProfile>
       <StPostDiv>
         <StMyPostdiv>
           <StPostListTitle>내가 쓴 게시물</StPostListTitle>
-          <PostList sorting="myPost" userId={params.userId} />
+          <PostList sorting='myPost' userId={userId} />
         </StMyPostdiv>
       </StPostDiv>
     </StMyPageWrapper>
