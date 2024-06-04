b357e76abd70ee3ce64325d4817732c7fe10c8a3
GA HYEON <dlrkxkszz@gmail.com>
2024-06-04 14:35:16 +0900

    수정완료

--- src/pages/Comment/Comment.jsx
+++ src/pages/Comment/Comment.jsx
@@ -1,19 +1,43 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import {
-    StCommentForm,
-    DetailForm,
+    StDetailPage,
+    StDetailForm,
+    StCommentWrapper,
+    StCommentTitle,
+    StUserProfileImg,
     StCommentWrite,
+    StWriteButton,
+    StCommentList,
 } from './Comment.styled';
 
 function comment ( ) {
+
+
     return (
         <>
             <Link to='./comment'></Link>
-            <StCommentForm>
-                <DetailForm> 상세페이지 입니다.</DetailForm>
-                <StCommentWrite>댓글 창 입니다.</StCommentWrite>
-            </StCommentForm>
+            <StDetailPage>
+                <StDetailForm> 상세페이지 입니다.</StDetailForm>
+                <StCommentWrapper> 
+                    <h1>Comment</h1>
+                    <StCommentTitle>
+                        <StUserProfileImg>
+                            {/* user 프로필 사진 들어가야함 */}
+                        </StUserProfileImg>
+                        <StCommentWrite>
+                            <p style={{fontSize: "20px"}}>username</p>
+                            <input 
+                            type="text" 
+                            placeholder="댓글을 입력하세요."
+                            style={{textAlign:"left", width: "90%", height: "80%", border: "none", outline: "none"}}>
+                            </input>
+                            <StWriteButton>등록</StWriteButton>
+                        </StCommentWrite>
+                    </StCommentTitle>
+                    <StCommentList>댓글 리스트 보여지는 곳</StCommentList>
+                </StCommentWrapper>
+            </StDetailPage>
         </>
     );
 }
--- src/pages/Comment/Comment.styled.js
+++ src/pages/Comment/Comment.styled.js
@@ -1,27 +1,78 @@
 import styled from "styled-components";
 
-const StCommentForm = styled.div`
+// 예진님 상세페이지 들어가야함
+const StDetailPage = styled.div`
     padding: 30px;
     margin: 30px;
     background-color: pink;
 `;
 
-const DetailForm = styled.div`
+const StDetailForm = styled.div`
     height: 1000px;
     padding: 30px;
     margin: 30px;
     background-color: skyblue;
 `;
 
-const StCommentWrite = styled.div`
+// 여기서부터 comment
+const StCommentWrapper = styled.div`
+    width: 60%;
     padding: 30px;
     margin: 30px;
-    bolder: 10px;
+    font-size: 40px;
+    font-weight: Bold;
+    border: 3px solid #C3C3C3;
+    border-radius: 10px;
     background-color: white;
+`;
 
-`
+const StCommentTitle = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px;
+    margin: 30px;
+    border: 3px solid #C3C3C3;
+    border-radius: 10px;
+`;
+
+const StUserProfileImg = styled.div`
+    border: 2px solid #C3C3C3;
+    border-radius: 50%;
+    width: 60px;
+    height: 60px;
+    padding: 10px;
+    margin: 10px;
+`;
+
+const StCommentWrite = styled.div`
+    border: 2px solid #C3C3C3;
+    border-radius: 10px;
+    width: 80%;
+    height: 100px;
+    padding: 10px;
+    margin: 10px;
+`;
+
+const StWriteButton = styled.button`
+    border: 1px solid #C3C3C3;
+    border-radius: 2px;
+    padding: 5px;
+`;
+
+const StCommentList = styled.div`
+    padding: 10px;
+    margin: 30px;
+    border: 3px solid #C3C3C3;
+    border-radius: 10px;    
+`;
 export {
-    StCommentForm,
-    DetailForm,
+    StDetailPage,
+    StDetailForm,
+    StCommentWrapper,
+    StCommentTitle,
+    StUserProfileImg,
     StCommentWrite,
+    StWriteButton,
+    StCommentList,
 };
