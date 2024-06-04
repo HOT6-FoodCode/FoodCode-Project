510bbe40f5049ee58ee8f9cb2f64c899f1ea1f0e
hyeonw8 <whgusrud223@gmail.com>
2024-06-04 17:18:56 +0900

    ✨ update : userId 넘겨주기
    내 게시물만 보기 위한 userId 넘겨주기

--- src/components/posts/PostList/PostList.jsx
+++ src/components/posts/PostList/PostList.jsx
@@ -1,10 +1,9 @@
 import { useEffect, useState } from 'react';
-
 import api from '../../../api/api';
 import PostItem from '../PostItem';
 import { PostGrid } from './PostList.styled';
 
-const PostList = ({ sorting }) => {
+const PostList = ({ sorting, userId }) => {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
@@ -14,14 +13,14 @@ const PostList = ({ sorting }) => {
       if (sorting === 'recent') {
         sortedPosts.sort((a, b) => new Date(b.created_at) - new Date(a.created_at));
       } else if (sorting === 'myPost') {
-        sortedPosts = sortedPosts.filter(post => post.id === 6); // 임시 조건 부여
+        sortedPosts = sortedPosts.filter(post => post.userId === userId); 
         //console.log(sortedPosts);
       }
       setPosts(sortedPosts);
     };
 
     getPosts();
-  }, [sorting]);
+  }, [sorting, userId]);
 
   return (
     <PostGrid>
