be2e6eb2c4e1e5790fef8097c6bdeab20f25d3bc
hyeonw8 <whgusrud223@gmail.com>
2024-06-05 03:59:57 +0900

    🩹 fix : userInfo 수정

--- src/components/ui/UserInfo/UserInfo.jsx
+++ src/components/ui/UserInfo/UserInfo.jsx
@@ -1,41 +1,38 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef } from 'react';
 import api from '../../../api/api';
-import { StUserImgUpdateDiv, StUserImgUpdateLabel, StUserImgUpdateInput,
-  StUserInfoContainer, StUserInfo, StUserContents, StUserLabel, StUserValue } from '../UserInfo/UserInfo.styled';
+import {
+  StUserImgUpdateDiv,
+  StUserImgUpdateLabel,
+  StUserImgUpdateInput,
+  StUserInfoContainer,
+  StUserInfo,
+  StUserContents,
+  StUserLabel,
+  StUserValue
+} from '../UserInfo/UserInfo.styled';
 import UserImg from './UserImg';
-const UserImgUpdate = ({userId}) => {
-const [userData, setUserData] = useState(null);
-console.log(userId)
-const fileInputRef = useRef(null);
-  
+import { useSelector, useDispatch } from 'react-redux';
+import { userDataUpdate } from '../../../redux/slices/userSlice';
+const UserImgUpdate = ({ userId, user }) => {
+  //const [userData, setUserData] = useState(null);
+  //console.log(userId);
+  const fileInputRef = useRef(null);
+  const userProfileData = useSelector((state) => state.user.userProfile);
+  console.log('상태확인, UserInfo', userProfileData);
+  const dispatch  = useDispatch();
 
   const fetchUserProfilePicture = async () => {
     try {
       const userInfo = await api.user.getUserProfile(userId);
-        console.log(userInfo)
-        setUserData(userInfo);
+      console.log(userInfo);
+      dispatch(userDataUpdate(userInfo));
     } catch (error) {
       console.error('프로필 사진 업로드 및 사용자 데이터 업데이트 오류:', error.message);
     }
-
   };
-  
-  
-  //console.log(imageUrl);
 
-  // const handleFileInputChange = async (files) => {
-  //   if (files.length === 0) return;
-
-  //   const profilePictureFile = files[0];
-
-  //   try {
-  //     await api.user.updateUserProfile(userId, profilePictureFile);
-  //     await fetchUserProfilePicture();
-  //   } catch (error) {
-  //     console.error('프로필 사진 업로드 및 사용자 데이터 업데이트 오류:', error.message);
-  //   }
-  // };
   const handleFileInputChange = async (file) => {
+
     try {
       await api.user.updateUserProfile(userId, file);
       await fetchUserProfilePicture();
@@ -44,40 +41,39 @@ const fileInputRef = useRef(null);
     }
   };
 
-  const imageUrl = userData && userData.profilePictureUrl;
+  const imageUrl = userProfileData && userProfileData.profilePictureUrl;
+
 
   useEffect(() => {
     fetchUserProfilePicture();
-    
-  }, [userId]);
+  }, [userId, user]);
 
-  
   return (
     <StUserInfoContainer>
       <div>
-      {imageUrl && <UserImg imgUrl={imageUrl}/>}
+        <UserImg imgUrl={imageUrl} />
         <StUserImgUpdateDiv>
-          <StUserImgUpdateLabel htmlFor='hiddenFileInput'>이미지 등록</StUserImgUpdateLabel>
+          <StUserImgUpdateLabel htmlFor="hiddenFileInput">이미지 등록</StUserImgUpdateLabel>
           <StUserImgUpdateInput
-            onChange={(e) => handleFileInputChange(e.target.files)}
+            onChange={(e) => handleFileInputChange(e.target.files[0])}
             type="file"
             ref={fileInputRef}
             id="hiddenFileInput"
           />
         </StUserImgUpdateDiv>
-      </div> 
+      </div>
       <StUserInfo>
         <StUserContents>
           <StUserLabel>닉네임</StUserLabel>
-          <StUserValue>{userData && userData.nickname}</StUserValue>
+          <StUserValue>{userProfileData && userProfileData.nickname}</StUserValue>
         </StUserContents>
         <StUserContents>
           <StUserLabel>이메일</StUserLabel>
-          <StUserValue>{userData && userData.email}</StUserValue>
+          <StUserValue>{user && user.email}</StUserValue>
         </StUserContents>
       </StUserInfo>
     </StUserInfoContainer>
-  )
-}
+  );
+};
 
-export default UserImgUpdate
+export default UserImgUpdate;
